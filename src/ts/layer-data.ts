d3e017ce7c7b22548c567806619080165324fbe6
Benjamin Root <benjamin.root@unlv.edu>
2016-04-19 13:15:57 -0700

    Genericizes the photos used in example app

--- src/ts/layer-data.ts
+++ src/ts/layer-data.ts
@@ -2,15 +2,15 @@ import {Layer} from './layer';
 
 export var layerData: Layer[] =[
   {
-    "tabOrder": 1,
-    "name": "River Yuman Subgroups",
-    "image": "./images/River-Yumans---past-territories_labels.png",
-    "imageAlt": "Map showing historical extent of River Yuman subgroups",
+    "tabOrder": 2,
+    "name": "Happy Kitten",
+    "image": "./images/happy-kitten.jpg",
+    "imageAlt": "Kitten showing joy",
   },
   {
-    "tabOrder": 2,
-    "name": "River Yuman Reservations",
-    "image": "./images/River-Yumans--modern-reservations_labeled.png",
-    "imageAlt": "Map showing current reservations for River Yuman cultural groups",
+    "tabOrder": 1,
+    "name": "Sad Kitten",
+    "image": "./images/i_are_sad_kitten.jpg",
+    "imageAlt": "The saddest kitten",
   }
 ]

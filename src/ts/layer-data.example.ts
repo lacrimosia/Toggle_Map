7e76bfae65a6290970b7f8520d94d6a5a7482cf6
Benjamin Root <benjamin.root@unlv.edu>
2016-04-19 14:05:35 -0700

    Uses home made kitten graphics

--- src/ts/layer-data.example.ts
+++ src/ts/layer-data.example.ts
@@ -4,13 +4,13 @@ export var layerData: Layer[] =[
   {
     "tabOrder": 2,
     "name": "Happy Kitten",
-    "image": "./images/happy-kitten.jpg",
+    "image": "./images/happy-kitten-01.jpg",
     "imageAlt": "Kitten showing joy",
   },
   {
     "tabOrder": 1,
     "name": "Sad Kitten",
-    "image": "./images/i_are_sad_kitten.jpg",
+    "image": "./images/sad-kitten-01.jpg",
     "imageAlt": "The saddest kitten",
   }
 ]

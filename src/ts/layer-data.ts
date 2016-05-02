96947ee953394b9b7e564b4c7df4055220c6e206
Shatilla Prayer <prayers@unlv.nevada.edu>
2016-05-02 10:17:09 -0700

    Added new images, and added new content

--- src/ts/layer-data.ts
+++ src/ts/layer-data.ts
@@ -3,14 +3,14 @@ import {Layer} from './layer';
 export var layerData: Layer[] =[
   {
     "tabOrder": 1,
-    "name": "Apache Past Territories",
-    "image": "./images/Apache-past-territories.png",
+    "name": "OOdham Past Territories",
+    "image": "./images/OOdham--past-territories.png",
     "imageAlt": "Map of the Southwest showing River Yuman subgroups",
   },
   {
     "tabOrder": 2,
-    "name": "Apache Modern Reservations",
-    "image": "./images/Apache-modern-reservations.png",
+    "name": "OOdham Modern Reservations",
+    "image": "./images/OOdham--modern-reservations.png",
     "imageAlt": "Map of the Southwest showing River Yuman subgroups",
   }
 ]

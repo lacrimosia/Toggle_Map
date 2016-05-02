b8b63ae098a2fd7f75d209c1786d8127ebe1281d
Benjamin Root <benjamin.root@unlv.edu>
2016-05-02 15:47:17 -0700

    Fix O'odham capitalization and spelling'

--- src/ts/layer-data.ts
+++ src/ts/layer-data.ts
@@ -3,13 +3,13 @@ import {Layer} from './layer';
 export var layerData: Layer[] =[
   {
     "tabOrder": 1,
-    "name": "O'Odham Past Territories",
+    "name": "O'odham Past Territories",
     "image": "./images/OOdham--past-territories.png",
     "imageAlt": "Map of the Southwest showing River Yuman subgroups",
   },
   {
     "tabOrder": 2,
-    "name": "O'Odham Modern Reservations",
+    "name": "O'odham Modern Reservations",
     "image": "./images/OOdham--modern-reservations.png",
     "imageAlt": "Map of the Southwest showing River Yuman subgroups",
   }

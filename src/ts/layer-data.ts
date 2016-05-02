959d2a1a6049c8051603273932d880ffc34d2894
Benjamin Root <benjamin.root@unlv.edu>
2016-05-02 15:42:16 -0700

    Add apostrophe to oodham name.

--- src/ts/layer-data.ts
+++ src/ts/layer-data.ts
@@ -3,13 +3,13 @@ import {Layer} from './layer';
 export var layerData: Layer[] =[
   {
     "tabOrder": 1,
-    "name": "OOdham Past Territories",
+    "name": "O'Odham Past Territories",
     "image": "./images/OOdham--past-territories.png",
     "imageAlt": "Map of the Southwest showing River Yuman subgroups",
   },
   {
     "tabOrder": 2,
-    "name": "OOdham Modern Reservations",
+    "name": "O'Odham Modern Reservations",
     "image": "./images/OOdham--modern-reservations.png",
     "imageAlt": "Map of the Southwest showing River Yuman subgroups",
   }
